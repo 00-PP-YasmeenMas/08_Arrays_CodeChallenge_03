1dd62d7860c7879f14ab51480444ba85bc339e84
Yasmeen <yasmeenmas@gmx.de>
2023-07-19 12:42:49 +0200

    Satzbau mit Array + Punktuation | Test

--- script_01.js
+++ script_01.js
@@ -12,18 +12,44 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-output(getSentence(["Bist","du","Max"],"Q"));
-output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+// output(getSentence(["Ich","bin","Max"],"S"));
+// output(getSentence(["Bist","du","Max"],"Q"));
+// output(getSentence(["Ich","bin"],"E"));
+// output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
-function getSentence(arr,op) {
+
+// Modul: Punktuation | Test
+output(getPunktuation("S"));
+output(getPunktuation("Q"));
+output(getPunktuation("E"));
+output(getPunktuation("ö"));
+function getPunktuation(op) {
    
+  switch (op) {
+    case "S":
+        op = ".";
+        break;
+    case "Q":
+        op = "?";
+        break;
+    case "E":
+        op = "!";
+        break;
+  
+    default:
+        op = "";
+        break;
+  } 
    
-   
-     return "toDo";
+     return op;
 }
 
+//Modul: Satzbau mit Array | Test
+// output(getSentenceArr(["Ich","bin","Max"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    return arr.join(GAP);
+}
 
 
 // Modul: Ausgabe in Konsole : Test
