d70287a5db005314256d05905f485c626d082b6e
Yasmeen <yasmeenmas@gmx.de>
2023-07-20 09:19:49 +0200

    Cagri's Lösung

--- script_01.js
+++ script_01.js
@@ -13,14 +13,28 @@
 /*** Funktion mit Array als Parameter */
 
 
+// Cagri's Lösung
+// const GAP = " ";
+// const PUNCTUATION = {S: '.', Q: '?', E: '!'};
+// const ERROR_PUNC = "FEHLER! Satzzeichen nicht erkannt.";
+// function getSentence(arr,op) {
+//     if (op in PUNCTUATION) {
+//         return arr.join(GAP) + PUNCTUATION[op];
+//     } else {
+//         return ERROR_PUNC;
+//     }
+// }
+
 //Modul: Array als Parameter
 output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+output(getSentence(["Ich","bin","Max"],"A"));
+
 function getSentence(arr, op) {
 
-    return getSentenceArr (arr) + getPunktuation (op);
+    return getSentenceArr(arr) + getPunktuation(op);
 }
 
 
